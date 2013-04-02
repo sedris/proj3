21df4574e905dde4eadb0eb64e8f8451da606c5b
Sarah Edris <sedris@mit.edu>
2013-04-02 14:55:46 -0400

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SedrisProj3::Application.config.secret_token = 'd346494de6b3fa6050f1c9c724aa3ab1d622497bad1740af2117d0c36808c7305600895decee17ace7d71b4ad6a0ff8cbcc6af7c274e45425201958678e79a78'
