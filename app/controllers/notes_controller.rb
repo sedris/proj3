3dbd60174e4ab09d711d2b72933a138d9034fc85
Sarah Edris <sedris@mit.edu>
2013-04-07 21:52:13 -0400

    can delete notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -85,6 +85,7 @@ class NotesController < ApplicationController
     respond_to do |format|
       format.html { redirect_to notes_url }
       format.json { head :no_content }
+      format.js { render :layout => false }
     end
   end
 end
