c4eebc3dcc3ce990346ebe6e35b9506e488e8fc1
Sarah Edris <sedris@mit.edu>
2013-04-08 23:46:36 -0400

    removed extra code

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,19 +1,7 @@
 class Note < ActiveRecord::Base
   attr_accessible :body, :height, :width, :x, :y
 
-  #validate :validate_attrs
   before_create :set_defaults
-
-	/# Requires: width > 0, x >= 0, y >= 0
-	# Modifies: None.
-	# Effects: 	Allows Note to save if valid, otherwise, does not save
-  def validate_attrs
-  	if self.width <= 0 or self.x < 0 or self.y < 0
-  		return false
-    end
-    
-  	return true
-  end/
 
   # Requires: None
   # Modifies: Attributes
@@ -25,18 +13,4 @@ class Note < ActiveRecord::Base
     self.x = self.x? ? self.x : 0
     self.y = self.y? ? self.y : 0
   end
-
-
-  # Requires: valid strings and ints
-  # Modifies: Attributes
-  # Effects:  Updates attributes
-  def update_note(body, width, height, x, y)
-    body = body? ? body : self.body
-    width = width? ? width : self.width
-    height = height? ? height : self.height
-    x = x? ? x : self.x
-    y = y? ? y : self.x
-    return self.update_attributes(:body => body, :width => width, :height => height, :x => x, :y =>y)
-  end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,27 +6,4 @@ class User < ActiveRecord::Base
 
 	has_many :notes, :dependent => :destroy
 
-	/# Requires: "name" is valid string
-	# Modifies: Boards associated with user
-	# Effects: 	Creates new board for user
-	# 			Board is named "Board [board_id]" if no name given
-	# 			Returns board
-	def add_board(name = '')
-		self.boards.create(:name => name)
-		if (name.empty?)
-			self.boards.last.update_attribute(:name, "Board " + self.boards.last.id.to_s())
-		end
-		return self.boards.last
-	end
-
-	# Requires: "board_id" valid int
-	# Modifies: Boards associated with user
-	# Effects: 	Destroys board and associated notes
-	def remove_board(board_id)
-		board = self.boards.find_by_id(board_id)
-		if (board)
-			board.notes.destroy_all()
-			self.boards.destroy(board_id)
-		end
-	end/
 end
